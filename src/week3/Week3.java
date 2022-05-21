de1cc2f17aecea73d08bec7cc2e1792f9cb09fcb
sin11889 <sin11889@sheridancollege.ca>
2022-05-21 19:47:45 -0400

    version 1

---
+++ src/week3/Week3.java
@@ -0,0 +1,19 @@
+package week3;
+
+
+/**
+ *
+ * @author Gurminder Singh
+ */
+
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Learning Github");
+    
+    }
+
+}
