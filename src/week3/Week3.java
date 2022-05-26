0c94b1914f71eeabd9827930cd956a84adbd8b67
sin11889 <105235434+sin11889@users.noreply.github.com>
2022-05-26 12:53:37 -0400

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -13,7 +13,6 @@ public class Week3 {
      */
     public static void main(String[] args) {
         System.out.println("Learning Github");
-        System.out.println("changes in github");
     
     }
 
